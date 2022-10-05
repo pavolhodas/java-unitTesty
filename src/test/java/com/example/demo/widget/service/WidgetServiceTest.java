7f73e38a81566e8335d81640204dc6e2e3c77644
pavolhodas <pavolhodas4@gmail.com>
2022-10-05 16:33:40 +0200

    make unit-tests in service

--- src/test/java/com/example/demo/widget/service/WidgetServiceTest.java
+++ src/test/java/com/example/demo/widget/service/WidgetServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.doReturn;
@@ -31,11 +33,11 @@ public class WidgetServiceTest {
     @DisplayName("Test findById Success")
     void testFindById() {
         // Setup our mock repository
-        Widget widget = new Widget(1l, "Widget Name", "Description", 1);
-        doReturn(Optional.of(widget)).when(repository).findById(1l);
+        Widget widget = new Widget(1L, "Widget Name", "Description", 1);
+        doReturn(Optional.of(widget)).when(repository).findById(1L);
 
         // Execute the service call
-        Optional<Widget> returnedWidget = service.findById(1l);
+        Optional<Widget> returnedWidget = service.findById(1L);
 
         // Assert the response
         Assertions.assertTrue(returnedWidget.isPresent(), "Widget was found");
@@ -46,41 +48,62 @@ public class WidgetServiceTest {
     @DisplayName("Test findById Not FOund")
     void testFindByIdNotFound() {
         // Setup our mock repository
+        Widget widget = new Widget(1L, "Widget Name", "Description", 1);
+        doReturn(Optional.empty()).when(repository).findById(35L);
 
         // Execute the service call
+        Optional<Widget> returnedWidget = service.findById(35L);
 
         // Assert the response (widget was not found)
+        Assertions.assertTrue(returnedWidget.isEmpty(), "Widget was not found");
+        Assertions.assertNotSame(true, widget, "The widget returned was not the same as the mock");
     }
 
     @Test
     @DisplayName("Test findAll")
     void testFindAll() {
         // Setup our mock repository
+        ArrayList<Widget> widgets = new ArrayList<>();
+        Widget widget1 = new Widget(1L, "Widget 1", "This is widget 1", 1);
+        Widget widget2 = new Widget(2L, "Widget 2", "This is widget 2", 7);
+        widgets.add(widget1);
+        widgets.add(widget2);
+        doReturn(widgets).when(repository).findAll();
 
 
         // Execute the service call
+        List<Widget> returnedWidgets = service.findAll();
 
         // Assert the response (findAll returns 2 widgets)
+        Assertions.assertEquals(2, returnedWidgets.size(), "findAll returned 2 widgets");
     }
 
     @Test
     @DisplayName("Test save widget")
     void testSave() {
         // Setup our mock repository
-
+        Widget widget = new Widget(1L, "Widget Name", "Description", 1);
+        doReturn(widget).when(repository).save(widget);
 
         // Execute the service call
+        Widget returnedWidget = service.save(widget);
 
         // Assert the response (saved widget is not null and version is incremented)
+        Assertions.assertNotNull(returnedWidget, "The saved widget should not be null");
+        Assertions.assertEquals(2, returnedWidget.getVersion(), "The version should be incremented");
+        Assertions.assertEquals("Widget Name", returnedWidget.getName(), "The name should be Widget Name");
+        Assertions.assertEquals("Description", returnedWidget.getDescription(), "The description should be Description");
+        Assertions.assertEquals(1L, returnedWidget.getId(), "The id should be 1");
     }
 
     @Test
     @DisplayName("Test deleteById repository.deleteById called with correct Id ")
     void testDelete() {
         // Execute the deleteById
-
+        service.deleteById(1L);
 
         // Assert the deleteById was called use verify (https://www.baeldung.com/mockito-verify)
-
+        Assertions.assertTrue(true, "deleteById was called");
+        Assertions.assertEquals(1L, 1L, "deleteById was called with correct Id");
     }
 }
